3b9d95330d64a8837f5280baeaa3d963f4226dc5
lihaoyun6 <lihaoyun11@gmail.com>
2025-09-18 17:02:21 +0800

    add TextEncodeQwenImageEdit (Advanced)

--- nodes.py
+++ nodes.py
@@ -1,11 +1,13 @@
 import os
 import io
+import math
 import json
 import torch
 import base64
 import dashscope
-
 import folder_paths
+import node_helpers
+import comfy.utils
 
 import numpy as np
 from PIL import Image
@@ -321,11 +323,55 @@ class QwenPromptRewriter:
             
         #print(f'Prompt: "{output_prompt}"')
         return (output_prompt,)
+
+class TextEncodeQwenImageEditAdv:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {
+            "clip": ("CLIP", ),
+            "prompt": ("STRING", {"multiline": True, "dynamicPrompts": True}),
+            },
+            "optional": {"vae": ("VAE", ),
+                         "image": ("IMAGE", ),
+                         "latent": ("LATENT", ),}}
+    
+    RETURN_TYPES = ("CONDITIONING",)
+    FUNCTION = "encode"
+    
+    CATEGORY = "advanced/conditioning"
+    
+    def encode(self, clip, prompt, vae=None, image=None, latent=None):
+        ref_latent = None
+        if image is None:
+            images = []
+        else:
+            samples = image.movedim(-1, 1)
+            total = int(1024 * 1024)
+            
+            scale_by = math.sqrt(total / (samples.shape[3] * samples.shape[2]))
+            width = round(samples.shape[3] * scale_by)
+            height = round(samples.shape[2] * scale_by)
+            
+            s = comfy.utils.common_upscale(samples, width, height, "area", "disabled")
+            image = s.movedim(1, -1)
+            images = [image[:, :, :, :3]]
+            if latent is not None:
+                ref_latent = latent
+            elif vae is not None:
+                ref_latent = vae.encode(image[:, :, :, :3])
+                
+        tokens = clip.tokenize(prompt, images=images)
+        conditioning = clip.encode_from_tokens_scheduled(tokens)
+        if ref_latent is not None:
+            conditioning = node_helpers.conditioning_set_values(conditioning, {"reference_latents": [ref_latent]}, append=True)
+        return (conditioning, )
     
 NODE_CLASS_MAPPINGS = {
-    "QwenPromptRewriter": QwenPromptRewriter
+    "QwenPromptRewriter": QwenPromptRewriter,
+    "TextEncodeQwenImageEditAdv": TextEncodeQwenImageEditAdv
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "QwenPromptRewriter": "Qwen Prompt Rewriter"
+    "QwenPromptRewriter": "Qwen Prompt Rewriter",
+    "TextEncodeQwenImageEditAdv": "TextEncodeQwenImageEdit (Advanced)"
 }
