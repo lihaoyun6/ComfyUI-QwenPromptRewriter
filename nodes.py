0b2179cbae869901f304f33ed4f789f78bd593ce
lihaoyun6 <lihaoyun11@gmail.com>
2025-09-23 16:45:09 +0800

    added TextEncodeQwenImageEditPlus (Advanced)

--- nodes.py
+++ nodes.py
@@ -330,17 +330,18 @@ class TextEncodeQwenImageEditAdv:
         return {"required": {
             "clip": ("CLIP", ),
             "prompt": ("STRING", {"multiline": True, "dynamicPrompts": True}),
+            "negative_prompt": ("STRING", ),
             },
             "optional": {"vae": ("VAE", ),
-                         "image": ("IMAGE", ),
-                         "latent": ("LATENT", ),}}
+                         "image": ("IMAGE", ),}}
     
-    RETURN_TYPES = ("CONDITIONING",)
+    RETURN_TYPES = ("CONDITIONING", "CONDITIONING", "LATENT",)
+    RETURN_NAMES = ("conditioning+", "conditioning-", "latent")
     FUNCTION = "encode"
     
     CATEGORY = "advanced/conditioning"
     
-    def encode(self, clip, prompt, vae=None, image=None, latent=None):
+    def encode(self, clip, prompt, negative_prompt, vae=None, image=None):
         ref_latent = None
         if image is None:
             images = []
@@ -353,26 +354,87 @@ class TextEncodeQwenImageEditAdv:
             height = round(samples.shape[2] * scale_by)
             
             s = comfy.utils.common_upscale(samples, width, height, "area", "disabled")
-            image = s.movedim(1, -1)
-            images = [image[:, :, :, :3]]
+            new_image = s.movedim(1, -1)
+            images = [new_image[:, :, :, :3]]
             
-            if latent is not None:
-                ref_latent = latent["samples"]
-            elif vae is not None:
+            if vae is not None:
                 ref_latent = vae.encode(image[:, :, :, :3])
                 
         tokens = clip.tokenize(prompt, images=images)
         conditioning = clip.encode_from_tokens_scheduled(tokens)
+        tokensN = clip.tokenize(negative_prompt, images=images)
+        conditioningN = clip.encode_from_tokens_scheduled(tokens)
         if ref_latent is not None:
             conditioning = node_helpers.conditioning_set_values(conditioning, {"reference_latents": [ref_latent]}, append=True)
-        return (conditioning, )
+        return (conditioning, conditioningN, {"samples":ref_latent}, )
+
+class TextEncodeQwenImageEditPlusAdv:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {
+            "clip": ("CLIP", ),
+            "prompt": ("STRING", {"multiline": True, "dynamicPrompts": True}),
+            "negative_prompt": ("STRING", ),
+            },
+            "optional": {"vae": ("VAE", ),
+                         "image1": ("IMAGE", ),
+                         "image2": ("IMAGE", ),
+                         "image3": ("IMAGE", ),
+                         }}
+    
+    RETURN_TYPES = ("CONDITIONING", "CONDITIONING", "LATENT",)
+    RETURN_NAMES = ("conditioning+", "conditioning-", "latent")
+    FUNCTION = "encode"
+    
+    CATEGORY = "advanced/conditioning"
+    
+    def encode(self, clip, prompt, negative_prompt, vae=None, image1=None, image2=None, image3=None):
+        ref_latents = []
+        images = [img for img in [image1, image2, image3] if img is not None]
+        images_vl = []
+        llama_template = "<|im_start|>system\nDescribe the key features of the input image (color, shape, size, texture, objects, background), then explain how the user's text instruction should alter or modify the image. Generate a new image that meets the user's requirements while maintaining consistency with the original input where appropriate.<|im_end|>\n<|im_start|>user\n{}<|im_end|>\n<|im_start|>assistant\n"
+        image_prompt = ""
+        
+        size = 1024
+        if len(images) > 2:
+            size = 384
+        elif len(images) > 1:
+            size = 512
+        
+        for i, image in enumerate(images):
+            samples = image.movedim(-1, 1)
+            total = int(size * size)
+            
+            scale_by = math.sqrt(total / (samples.shape[3] * samples.shape[2]))
+            width = round(samples.shape[3] * scale_by)
+            height = round(samples.shape[2] * scale_by)
+            
+            s = comfy.utils.common_upscale(samples, width, height, "area", "disabled")
+            images_vl.append(s.movedim(1, -1))
+            if vae is not None:
+                ref_latents.append(vae.encode(image[:, :, :, :3]))
+                
+            image_prompt += "Picture {}: <|vision_start|><|image_pad|><|vision_end|>".format(i + 1)
+                
+        tokens = clip.tokenize(image_prompt + prompt, images=images_vl, llama_template=llama_template)
+        conditioning = clip.encode_from_tokens_scheduled(tokens)
+        tokensN = clip.tokenize(image_prompt + negative_prompt, images=images_vl, llama_template=llama_template)
+        conditioningN = clip.encode_from_tokens_scheduled(tokensN)
+        
+        if len(ref_latents) > 0:
+            conditioning = node_helpers.conditioning_set_values(conditioning, {"reference_latents": ref_latents}, append=True)
+            return (conditioning, conditioningN, {"samples": ref_latents[0]}, )
+        
+        return (conditioning, conditioningN, {"samples": None}, )
     
 NODE_CLASS_MAPPINGS = {
     "QwenPromptRewriter": QwenPromptRewriter,
-    "TextEncodeQwenImageEditAdv": TextEncodeQwenImageEditAdv
+    "TextEncodeQwenImageEditAdv": TextEncodeQwenImageEditAdv,
+    "TextEncodeQwenImageEditPlusAdv": TextEncodeQwenImageEditPlusAdv
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "QwenPromptRewriter": "Qwen Prompt Rewriter",
-    "TextEncodeQwenImageEditAdv": "TextEncodeQwenImageEdit (Advanced)"
+    "TextEncodeQwenImageEditAdv": "TextEncodeQwenImageEdit (Advanced)",
+    "TextEncodeQwenImageEditPlusAdv": "TextEncodeQwenImageEditPlus (Advanced)"
 }
