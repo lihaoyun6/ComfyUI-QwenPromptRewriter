fc02ec1829c91229ee659760f90aa4acbc4cf13c
lihaoyun6 <lihaoyun11@gmail.com>
2025-09-23 17:57:17 +0800

    add more options

--- nodes.py
+++ nodes.py
@@ -331,6 +331,7 @@ class TextEncodeQwenImageEditAdv:
             "clip": ("CLIP", ),
             "prompt": ("STRING", {"multiline": True, "dynamicPrompts": True}),
             "negative_prompt": ("STRING", ),
+            "align_latent": ("BOOLEAN", {"default": True}),
             },
             "optional": {"vae": ("VAE", ),
                          "image": ("IMAGE", ),}}
@@ -341,7 +342,7 @@ class TextEncodeQwenImageEditAdv:
     
     CATEGORY = "advanced/conditioning"
     
-    def encode(self, clip, prompt, negative_prompt, vae=None, image=None):
+    def encode(self, clip, prompt, negative_prompt, align_latent, vae=None, image=None):
         ref_latent = None
         if image is None:
             images = []
@@ -358,7 +359,10 @@ class TextEncodeQwenImageEditAdv:
             images = [new_image[:, :, :, :3]]
             
             if vae is not None:
-                ref_latent = vae.encode(image[:, :, :, :3])
+                if align_latent:
+                    ref_latent = vae.encode(image[:, :, :, :3])
+                else:
+                    ref_latent = vae.encode(new_image[:, :, :, :3])
                 
         tokens = clip.tokenize(prompt, images=images)
         conditioning = clip.encode_from_tokens_scheduled(tokens)
@@ -375,6 +379,8 @@ class TextEncodeQwenImageEditPlusAdv:
             "clip": ("CLIP", ),
             "prompt": ("STRING", {"multiline": True, "dynamicPrompts": True}),
             "negative_prompt": ("STRING", ),
+            "smart_resize": ("BOOLEAN", {"default": True}),
+            "align_latent": ("BOOLEAN", {"default": True}),
             },
             "optional": {"vae": ("VAE", ),
                          "image1": ("IMAGE", ),
@@ -388,18 +394,20 @@ class TextEncodeQwenImageEditPlusAdv:
     
     CATEGORY = "advanced/conditioning"
     
-    def encode(self, clip, prompt, negative_prompt, vae=None, image1=None, image2=None, image3=None):
+    def encode(self, clip, prompt, negative_prompt, smart_resize, align_latent, vae=None, image1=None, image2=None, image3=None):
         ref_latents = []
         images = [img for img in [image1, image2, image3] if img is not None]
         images_vl = []
         llama_template = "<|im_start|>system\nDescribe the key features of the input image (color, shape, size, texture, objects, background), then explain how the user's text instruction should alter or modify the image. Generate a new image that meets the user's requirements while maintaining consistency with the original input where appropriate.<|im_end|>\n<|im_start|>user\n{}<|im_end|>\n<|im_start|>assistant\n"
         image_prompt = ""
         
-        size = 1024
-        if len(images) > 2:
-            size = 384
-        elif len(images) > 1:
-            size = 512
+        size = 384
+        if smart_resize:
+            size = 1024
+            if len(images) > 2:
+                size = 384
+            elif len(images) > 1:
+                size = 512
         
         for i, image in enumerate(images):
             samples = image.movedim(-1, 1)
@@ -412,7 +420,16 @@ class TextEncodeQwenImageEditPlusAdv:
             s = comfy.utils.common_upscale(samples, width, height, "area", "disabled")
             images_vl.append(s.movedim(1, -1))
             if vae is not None:
-                ref_latents.append(vae.encode(image[:, :, :, :3]))
+                if align_latent:
+                    ref_latents.append(vae.encode(image[:, :, :, :3]))
+                else:
+                    total = int(1024 * 1024)
+                    scale_by = math.sqrt(total / (samples.shape[3] * samples.shape[2]))
+                    width = round(samples.shape[3] * scale_by / 8.0) * 8
+                    height = round(samples.shape[2] * scale_by / 8.0) * 8
+                    
+                    s = comfy.utils.common_upscale(samples, width, height, "area", "disabled")
+                    ref_latents.append(vae.encode(s.movedim(1, -1)[:, :, :, :3]))
                 
             image_prompt += "Picture {}: <|vision_start|><|image_pad|><|vision_end|>".format(i + 1)
                 
